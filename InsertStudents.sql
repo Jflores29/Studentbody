e8edb275eee32957ae58c09e75275e5b79d4f6db
Jflores29 <jflores71814@yahoo.com>
2025-10-29 15:08:56 +0000

    Insert Script
    Script to insert data into mysql db once the DB and table are created

---
+++ InsertStudents.sql
@@ -0,0 +1,14 @@
+INSERT INTO Students (STUDENTID,FNAME,LNAME,MAJOR,GPA)
+VALUES 
+(5002, 'Mary', 'Gram', 'Pyschology',3.6),
+(7002, 'Joseph', 'Torro', 'Accounting', 3.9),
+(5442, 'Finn', 'Jones', 'Biology', 3.7),
+(5001, 'James', 'Bond', 'Business',3.5),
+(7005, 'Jane', 'Doe', 'Engineering', 4.0),
+(5412, 'Tiffany', 'Toast', 'Computer Science', 3.7),
+(5015, 'Matthew', 'Durham', 'Architecture',3.5),
+(2678, 'Joseph', 'Torro', 'Civil Engineering', 3.8),
+(2212, 'Lindsay', 'Lauren', 'Criminology', 4.0),
+(2670, 'Clark', 'Kent', 'Journalism', 3.8),
+(3111, 'Nancy', 'Night', 'Nutrition', 4.0)
+;
