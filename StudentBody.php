350dc135cf5074bd911e6f3cb68609a3854d4129
Jflores29 <jflores71814@yahoo.com>
2025-10-29 15:07:46 +0000

    Fix database connection and query execution issues

--- StudentBody.php
+++ StudentBody.php
@@ -1,44 +1,44 @@
-
+<!DOCTYPE html>
 <html>
 <head>
     <Title>Student Body </Title>
 </head>
 <body>
 <?php
-//This file should connect to
-
-$servername = "localhost"; //server name for SQL database
+//This file should connect to a mysql database return data based on a query
+$servername = "locahost:3306"; //server name for SQL database
 $username = "sbadmin"; //database username
 $password = "W3@r3UB716!"; //password for database user
 
 // Created a connection to the MYSQL Database.
 // There also functions that can connect to Oracle or MSSQL as well
-$dbname = "StudentBodyDB"
-$dbconn = new mysqli($servername, $username, $password,$dbname);
-
+$db = "StudentBodyDB";
+$dbconn = new mysqli($servername, $username, $password, $db);
 
 //Check if connection was successful
-if($dbconn->connect_error){
+if(!$dbconn){
     die("Connection failed: ". $dbconn->connect_error);
 }
+else{ 
+    echo "Connection Success!";
+}
+$query = "Select * FROM Students ORDER BY GPA DESC LIMIT 10;";
 
-$query = "Select * FROM Students ORDER BY GPA DESC LIMIT 10";
-
-$qresult = $dbconn->query($query);// run the query
+$qresult = $dbconn->execute_query($query,$dbconn);// run the query
 
 //Check if there any rows from the query result and output the results
 if ($qresult->num_rows > 0){
     //output data of each row into a table
     echo "<table> <tr><th>First Name</th><th>Last Name</th><th>Major</th><th>GPA</th><th>Student ID</th></tr>";
     while($row = $qresult->fetch_assoc()){
-        echo "<tr><td>".$row['FNAME']."</td><td>".$row['LNAME']."</td><td>".$row['MAJOR']."</td><td>".$row['GPA']."</td><td>" .$row['STUDENTID']."</td><tr>";
+        echo "<tr><td>".$row['FNAME']."</td><td>".$row['LNAME']."</td><td>".$row['MAJOR']."</td><td>".$row['GPA']."</td><td>" .$row['STUDENTID']."</td><td>";
     }
     echo "</table>";
 }
 else{
     echo "No Students in your Student Body database!";
 }
-$dbconn->close(); //Close the connection to the database
+mysqli_close($dbconn); //Close the connection to the database 
 ?>
 
 </body>
